d1b521e6fe4022d478e7a023c295fc2cad315890
Marek Kowalik <mkowalik92@gmail.com>
2018-11-06 17:17:57 -0500

    Code added for creating window.

--- simple-window.cpp
+++ simple-window.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h> // printf
 #include <unistd.h> // sleep
+#include <string>
 
 #include <GLFW/glfw3.h>
 
@@ -8,21 +9,24 @@ bool running = true;
 int window_width = 1024;
 int window_height = 768;
 
+
+std::string engine_version = "Game Engine v0.1";
+
 bool create_main_window( int windowWidth, int windowHeight );
 void clean_up();
 
 int main()
 {
   int time_running_sec = 0;
-  
-  printf( "Game Engine v0.1\n" );
-  
+
+  printf( (engine_version + "\n").c_str() );
+
   // Create Main Window
   if ( !create_main_window( window_width, window_height ) )
     {
       printf( "Failed to create main window!\n" );
     }
-  
+
   // Start Game Engine
   while ( running )
     {
@@ -49,7 +53,10 @@ bool create_main_window( int windowWidth, int windowHeight )
   glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 ); // Set minimum OpenGl Version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3 ); // Set maximum OpenGl Version
   glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-  
+
+  GLFWwindow* window;
+  window = glfwCreateWindow(windowWidth, windowHeight, engine_version.c_str(), NULL, NULL);
+
   return true;
 }
 
